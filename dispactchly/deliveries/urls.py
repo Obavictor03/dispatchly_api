09c76cef5767b526e27a9ae2050a391091a07b30
Obavictor03 <obarovictor03@gmail.com>
2026-02-28 13:23:37 +0100

    rider and sender endpoints

--- dispactchly/deliveries/urls.py
+++ dispactchly/deliveries/urls.py
@@ -4,11 +4,11 @@ from .views import (CancelDeliveryView, CreateDeliveryView, MyDeliveriesView, De
 urlpatterns = [
     path('', CreateDeliveryView.as_view()),
     path('my/', MyDeliveriesView.as_view()),
-    path('<int=pk>/', DeleveryDetailView.as_view()),
-    path('<int=pk>/cancel', CancelDeliveryView.as_view()),
+    path('<int:pk>/', DeleveryDetailView.as_view()),
+    path('<int:pk>/cancel', CancelDeliveryView.as_view()),
     path('available/', AvailableDeliveriesView.as_view()),
-    path('<int=pk>/accept/', AcceptDeliveryView.as_view()),
-    path('<int=pk>/status/', UpdateDeliveryStatusView.as_view()),
+    path('<int:pk>/accept/', AcceptDeliveryView.as_view()),
+    path('<int:pk>/status/', UpdateDeliveryStatusView.as_view()),
     path('rider/my-deliveries/', RiderDeliveryView.as_view()),
     path('rider/availablity/', UpdateAvailabilityView.as_view()),
 ]
